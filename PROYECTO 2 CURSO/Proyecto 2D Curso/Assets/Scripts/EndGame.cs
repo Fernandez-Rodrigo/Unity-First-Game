db872e54ed455ef396336a6021624c840e984809
Rodrigo Fernandez <rodrigoo.fernandez@gmail.com>
2020-08-06 16:37:25 -0300

    First Game

---
+++ PROYECTO 2 CURSO/Proyecto 2D Curso/Assets/Scripts/EndGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.tag == "Player")
+        {
+            GameManager.sharedInstance.currentGameState = GameState.end;
+          
+        }
+    }
+}
---
+++ PROYECTO 2 CURSO/Proyecto 2D Curso/Assets/Scripts/End_Level_Zone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End_Level_Zone : MonoBehaviour
+{
+
+    public GameObject Level3;
+
+    private void Update()
+    {
+        if(GameManager.sharedInstance.currentGameState == GameState.gameOver)
+        {
+            Destroy(GameObject.Find("Level_3(Clone)"));
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        Instantiate(Level3);
+
+    }
+
+}
+
